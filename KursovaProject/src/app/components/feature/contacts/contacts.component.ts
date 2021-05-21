73e6289f0d05d09d69a23dd1ab499ecd14cb1620
roomaank <roman.kushka10@gmail.com>
2021-05-22 00:39:57 +0300

    init

--- KursovaProject/src/app/components/feature/contacts/contacts.component.ts
+++ KursovaProject/src/app/components/feature/contacts/contacts.component.ts
@@ -9,7 +9,15 @@ export class ContactsComponent implements OnInit {
 
   constructor() { }
 
+  role = true;
+
   ngOnInit(): void {
   }
 
+  isChangedCheck(e: Event):void {
+    // @ts-ignore
+    const value = e.target.value;
+    this.role = !this.role;
+    console.log(this.role)
+  }
 }
